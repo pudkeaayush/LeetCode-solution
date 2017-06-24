b57e1edbd6c78cb085c106860b74fcdb2ae7d6a3
Raju Khanal <pudkeaayush@gmail.com>
2017-06-24 19:07:01 -0400

    Create LCA.java

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree/LCA.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while( true ) {
+            if ( root.val < p.val && root.val < q.val)
+                root = root.right;
+            else if( root.val > p.val && root.val > q.val)
+                root = root.left;
+            else
+                return root;
+        }
+    }
+}
