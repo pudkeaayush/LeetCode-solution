a6a9a8de91bf2e9a9da5bd2eaceca33e5549ba87
Raju Khanal <pudkeaayush@gmail.com>
2017-07-20 15:48:52 -0400

    Create Solution.java

---
+++ 7. Reverse Integer/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int reverse(int x) {
+        long ans = 0;
+        while ( x != 0 ) {
+            ans = ans * 10 + (x % 10);
+            if( ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE ) {
+                return 0;
+            }
+            x = x / 10;
+            //System.out.println(ans + " " + x);
+        }
+        return (int)ans;
+    }
+}
