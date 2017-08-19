a3dff77a0902f4de8c8e44e2cecb37aa5978d480
Raju Khanal <pudkeaayush@gmail.com>
2017-08-19 17:18:41 -0400

    Create Solution.java

---
+++ 442. Find All Duplicates in an Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List < Integer > ans = new ArrayList < Integer >();
+        for( int i = 0; i < nums.length; i++) {
+            if(nums[Math.abs(nums[i]) - 1] > 0)
+                nums[Math.abs(nums[i]) - 1] = nums[ Math.abs(nums[i]) - 1] * -1;
+            else
+                ans.add(Math.abs(nums[i]));
+            
+        }
+        return ans;
+    }
+}
