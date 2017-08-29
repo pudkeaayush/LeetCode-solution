4f27f98ecd45ae2709ccc046ebd08797892c7a53
Raju Khanal <pudkeaayush@gmail.com>
2017-08-29 08:30:06 -0400

    Update Solution.java

--- 341. Flatten Nested List Iterator/Solution.java
+++ 341. Flatten Nested List Iterator/Solution.java
@@ -1,5 +1,28 @@
-The idea here is to use a stack to store the elements from the end to front. In the hasNext() function, we first check 
-whether the current element in the stack is a single integer by using the isInteger() API of NestedInteger interface.
-If so , we simply return true. Else we keep popping the element from the stack and flatten it at each iteration.
-To flatten the list simply iterate from the size of the nested list by using the getList().size() and adding elements
-from the end to front. When the next() function is called, we thus simply need to pop and return the element. 
+public class NestedIterator implements Iterator<Integer> {
+    
+    Stack<NestedInteger> stack = new Stack<>();
+    public NestedIterator(List<NestedInteger> nestedList) {
+        for(int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+    
+    @Override
+    public Integer next() {
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while( !stack.isEmpty() ) {
+            NestedInteger top = stack.peek();
+            if(top.getInteger() != null)
+                return true;
+            stack.pop();
+            for( int i = top.getList().size() - 1; i >= 0; i--)
+                stack.push(top.getList().get(i));
+            
+        }
+        return false;
+    }
+}
