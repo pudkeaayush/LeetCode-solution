6191cb5a0ed7e622c03aaff19ed4bd012a3460a6
Raju Khanal <pudkeaayush@gmail.com>
2017-09-02 20:30:13 -0400

    Create Solution.java

---
+++ 261. Graph Valid Tree/Solution.java
@@ -0,0 +1,5 @@
+The basic idea here is to check :
+- Whether there is a cycle in the graph or not? If there is, that means it cannot form a tree.
+- Whether the length of the edges is equal to n - 1 i.e make sure all nodes are reachable.
+
+Union find can be used for detecting cycle.
