e074d37fe1581db7c93a26c6b3455c887fb5d25d
Raju Khanal <pudkeaayush@gmail.com>
2017-09-04 09:29:12 -0400

    Create Solution.java

---
+++ 206. Reverse Linked List/Solution.java
@@ -0,0 +1,12 @@
+public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while( curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
